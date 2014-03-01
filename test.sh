7a25b7b343741c4e1e380cd208ace7427a88ca5a
Jonathan Garzon <jogarzon@cisco.com>
2014-03-01 14:57:03 -0800

    modified:   test.sh

--- test.sh
+++ test.sh
@@ -3,16 +3,19 @@
 set -x
 set -e
 
-./dump_last_report_csv.py https://mate:matesw@mate-nvsdemo.cisco.com:8443 32
-./dump_props.py https://mate:matesw@mate-nvsdemo.cisco.com:8443 > props.txt
-cat props.txt
-./dump_myreports.py https://mate:matesw@mate-nvsdemo.cisco.com:8443 > reports.txt
-cat reports.txt
-./explore.py https://mate:matesw@mate-nvsdemo.cisco.com:8443 LSPs -filter "ActualPath(AP_HK_BB1|ae0.0)" -properties SourceNode,ActualPath,SetupBW,Traff -count 5
-./get_plan.py https://admin:cariden@mldev01:8443 -dir /tmp/
+read_server="https://mate:matesw@mate-nvsdemo.cisco.com:8443"
+write_server="https://admin:cariden@mldev01:8443"
+props_file=/tmp/props.txt
+reportsdef_file=/tmp/reports.txt
 
-./clean_myreports.py https://admin:cariden@mldev01:8443
-./clean_props.py https://admin:cariden@mldev01:8443
-./load_myreports.py https://admin:cariden@mldev01:8443 reports.txt
-./load_props.py https://admin:cariden@mldev01:8443 props.txt
-./run_myreports.py https://admin:cariden@mldev01:8443
+./dump_last_report_csv.py $read_server 32
+./dump_props.py $read_server > $props_file; cat $props_file
+./dump_myreports.py $read_server > $reportsdef_file; cat $reportsdef_file
+./explore.py $read_server LSPs -filter "ActualPath(AP_HK_BB1|ae0.0)" -properties SourceNode,ActualPath,SetupBW,Traff -count 5
+./get_plan.py $write_server -dir /tmp/
+
+./clean_myreports.py $write_server
+./clean_props.py $write_server
+./load_myreports.py $write_server $reportsdef_file
+./load_props.py $write_server $props_file
+./run_myreports.py $write_server
