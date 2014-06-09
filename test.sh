9b8fedb7ee62158dfd7bb52c95248b2e57a4ea3a
Thomas Holbrook <thomas.j.holbrook@cisco.com>
2014-06-09 09:35:56 -0400

    further parameterize test.sh

--- test.sh
+++ test.sh
@@ -10,14 +10,14 @@ else
     set -e
 fi
 
-$python ./dump_last_report_csv.py $read_server 32
+$python ./dump_last_report_csv.py $read_server $definition_id
 $python ./dump_props.py $read_server > $props_file; cat $props_file
 $python ./dump_myreports.py $read_server > $reportsdef_file; cat $reportsdef_file
-$python ./explore.py $read_server LSPs -filter 'ActualPath(AP_HK_BB1|ae0.0)' -properties SourceNode,ActualPath,SetupBW,Traff -count 5
+$python ./explore.py $read_server LSPs -filter $explore_filter -properties SourceNode,ActualPath,SetupBW,Traff -count 5
 $python ./get_plan.py $read_server -dir /tmp
-$python ./time-series.py $read_server Interfaces TraffIn 'AM_LA_BB2|TenGigE0/2/2' 130719_0000_UTC 130719_0200_UTC 
+$python ./time-series.py $read_server Interfaces TraffIn $time_series_keys $time_series_date1 $time_series_date2
 $python ./show_report.py $read_server -columns 1,2,3 1 -csv
-$python ./show_report.py $read_server 17 -filtered
+$python ./show_report.py $read_server $job_id -filtered
 
 $python ./clean_myreports.py $write_server
 $python ./clean_props.py $write_server
